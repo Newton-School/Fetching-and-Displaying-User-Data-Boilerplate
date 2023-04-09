c8e501263879ea27566d40653edf76cf25349f2c
Alok shukla <46757876+shukla505@users.noreply.github.com>
2023-04-09 10:51:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,40 @@
-import React from "react";
-import "../styles/App.css";
-import UserTable from "./UserTable";
 const App = () => {
+	const [users, setUsers] = useState([]);
+	useEffect(()=>{
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then((response)=>{
+			return response.json();
+		})
+		.then((result)=>{
+			// console.log(result)
+			setUsers(result);
+		})
+		.catch((err)=>{
+			console.error(err);
+		})
+	},[])
 	return (
 		<div id="main">
-			<UserTable />;
+			<table>
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>Name</th>
+						<th>Email</th>
+						<th>Phone</th>
+						<th>Website</th>
+					</tr>
+				</thead>
+				<tbody>
+					{
+						users.map((user)=>(
+							<tr key={user.id}>
+								<UserTable user={user} />
+							</tr>
+						))
+					}
+				</tbody>
+			</table>
 		</div>
 	);
 };
