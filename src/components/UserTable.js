2a80e2657601dd3705e5748d5338e46abb1a3c94
Alok shukla <46757876+shukla505@users.noreply.github.com>
2023-04-09 10:52:07 +0530

    Update UserTable.js

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -1,5 +1,16 @@
 import React, { useState, useEffect } from "react";
 
-function UserTable() {}
+function UserTable(props) {
+    const {id, name, email, phone, website} = props.user;
+    return(
+        <>
+            <td>{id}</td>
+            <td>{name}</td>
+            <td>{email}</td>
+            <td>{phone}</td>
+            <td>{website}</td>
+        </>
+    )
+}
 
 export default UserTable;
